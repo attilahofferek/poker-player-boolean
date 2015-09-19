7627703a49c8ee0d0e21d223b634288459a17498
Karácsony Máté <k_mate@inf.elte.hu>
2015-09-19 16:12:50 +0200

    gfdg

--- player.php
+++ player.php
@@ -6,44 +6,6 @@ class Player {
 	const RAINMAN_URL = 'http://rainman.leanpoker.org/rank';
 
 	public function betRequest($game_state) {
-		/*$myCards = $this->myCards($game_state);
-		$smallBlind = $game_state['small_blind'];
-		$bigBlind = $smallBlind * 2;
-		$playersCount = $this->countActivePlayers($game_state);
-		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$game_state["in_action"]]['bet'];
-		if (
-				(
-					$myCards[0]['rank'] == $myCards[1]['rank'] or
-					(in_array($myCards[0]['rank'], array('10', 'J', 'Q', 'K', 'A')) and
-					in_array($myCards[1]['rank'], array('10', 'J', 'Q', 'K', 'A')))
-				)
-				and $playersCount <= 3
-		) {
-			return 10000000;
-		}
-
-		if ($moneyNeedsToCall <= $bigBlind) {
-			return $moneyNeedsToCall;
-		}
-
-		return 0;*/
-
-		/*
-		$myCards = $this->myCards($game_state);
-		$smallBlind = $game_state['small_blind'];
-		$bigBlind = $smallBlind * 2;
-		$playersCount = $this->countActivePlayers($game_state);
-		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$game_state["in_action"]]['bet'];
-
-		if ($this->getHandClass($myCards) >= 3 && $playersCount <= 3) {
-			return 10000000;
-		}
-
-		if (false && $moneyNeedsToCall <= $bigBlind) {
-			return $moneyNeedsToCall;
-		}
-
-		return 0;*/
 		if (count($game_state["community_cards"]) < 3) {
       return $this->preFlop($game_state);
     } else {
@@ -68,10 +30,6 @@ class Player {
 
 		if ($this->getHandClass($myCards) >= 3 && $playersCount <= 3) {
 			return 10000000;
-		}
-
-		if ($moneyNeedsToCall <= $bigBlind) {
-			return $moneyNeedsToCall;
 		}
 
 		$isClassTwo = ($this->getHandClass($myCards) === 2);
