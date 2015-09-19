44296e988d50060d56fab1f30c7520d22694dc47
Attila Hofferek <admin@Endor.local>
2015-09-19 10:26:22 +0200

    commit

--- player.php
+++ player.php
@@ -7,6 +7,9 @@ class Player
     public function betRequest($game_state)
     {
 		$myCards = $this->myCards($game_state);
+		$smallBlind = $game_state['small_blind'];
+		$bigBlind = $smallBlind * 2;
+		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$gameState["in_action"]]['bet'];
 		if (
 				$myCards[0]['rank'] == $myCards[1]['rank'] or
 				in_array($myCards[0]['rank'], array('10', 'J', 'Q', 'K', 'A')) or
@@ -14,8 +17,12 @@ class Player
 		) {
 					return 10000000;
 		}
+
+		if ($moneyNeedsToCall <= $bigBlind) {
+			return $moneyNeedsToCall;
+		}
+		
 		return 0;
-        
     }
 
     public function showdown($game_state)
