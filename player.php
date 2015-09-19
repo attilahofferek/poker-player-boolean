7fe372aa3f455d8eaa8546a1dfaabefb1262cb7e
Attila Hofferek <admin@Endor.local>
2015-09-19 10:47:40 +0200

    commit

--- player.php
+++ player.php
@@ -24,6 +24,32 @@ class Player
 		
 		return 0;
     }
+	
+	public function betRequest2($game_state)
+    {
+		$myCards = $this->myCards($game_state);
+		$smallBlind = $game_state['small_blind'];
+		$bigBlind = $smallBlind * 2;
+		$playersCount = $this->countActivePlayers($game_state);
+		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$game_state["in_action"]]['bet'];
+		if (
+				(
+					$myCards[0]['rank'] == $myCards[1]['rank'] or
+					in_array($myCards[0]['rank'], array('10', 'J', 'Q', 'K', 'A')) or
+					in_array($myCards[1]['rank'], array('10', 'J', 'Q', 'K', 'A'))
+				) and (
+					$playersCount <= 3
+				)
+		) {
+					return 10000000;
+		}
+
+		if ($moneyNeedsToCall <= $bigBlind) {
+			return $moneyNeedsToCall;
+		}
+		
+		return 0;
+    }
 
     public function showdown($game_state)
     {
@@ -34,4 +60,11 @@ class Player
 		
 		return $gameState["players"][$gameState["in_action"]]["hole_cards"];
     }
+	
+	public function countActivePlayers($game_state)
+	{
+		array_filter($game_state, function($item) {
+			return $item['status'] === 'active';
+		});
+	}
 }
