f5a0df20faa3de19331811e068ee89619430c124
Karácsony Máté <k_mate@inf.elte.hu>
2015-09-19 11:28:18 +0200

    deploy

--- player.php
+++ player.php
@@ -6,23 +6,28 @@ class Player
 
     public function betRequest($game_state)
     {
-		$myCards = $this->myCards($game_state);
-		$smallBlind = $game_state['small_blind'];
-		$bigBlind = $smallBlind * 2;
-		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$game_state["in_action"]]['bet'];
-		if (
-				$myCards[0]['rank'] == $myCards[1]['rank'] or
-				in_array($myCards[0]['rank'], array('10', 'J', 'Q', 'K', 'A')) or
-				in_array($myCards[1]['rank'], array('10', 'J', 'Q', 'K', 'A'))
-		) {
-					return 10000000;
-		}
+      $myCards = $this->myCards($game_state);
+  		$smallBlind = $game_state['small_blind'];
+  		$bigBlind = $smallBlind * 2;
+  		$playersCount = $this->countActivePlayers($game_state);
+  		$moneyNeedsToCall = $game_state['current_buy_in'] - $game_state["players"][$game_state["in_action"]]['bet'];
+  		if (
+  				(
+  					$myCards[0]['rank'] == $myCards[1]['rank'] or
+  					in_array($myCards[0]['rank'], array('10', 'J', 'Q', 'K', 'A')) or
+  					in_array($myCards[1]['rank'], array('10', 'J', 'Q', 'K', 'A'))
+  				) and (
+  					$playersCount <= 3
+  				)
+  		) {
+  					return 10000000;
+  		}
 
-		if ($moneyNeedsToCall <= $bigBlind) {
-			return $moneyNeedsToCall;
-		}
+  		if ($moneyNeedsToCall <= $bigBlind) {
+  			return $moneyNeedsToCall;
+  		}
 
-		return 0;
+  		return 0;
     }
 
 	public function betRequest2($game_state)
